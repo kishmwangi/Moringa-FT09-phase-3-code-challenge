adcb18a466f098fa1d1e6d17e80ec1ecdbc23b4a
kishmwangi <kishmwangi31@gmail.com>
2024-06-07 17:38:58 +0300

    magazine

--- models/magazine.py
+++ models/magazine.py
@@ -1,8 +1,29 @@
 class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
+    def __init__(self, id, name, category=None):
+        self._id = id
+        self._name = name
+        self._category = category
+        
+    @property
+    def id(self):
+        return self._id
 
-    def __repr__(self):
-        return f'<Magazine {self.name}>'
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) < 2 or len(value) > 16:
+            raise ValueError("Name must be a string between 2 and 16 characters")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Category must be a non-empty string")
+        self._category = value
